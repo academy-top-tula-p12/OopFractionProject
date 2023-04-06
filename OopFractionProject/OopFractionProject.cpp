8c21fc89719cde32e18aa0b61ac5353c316580cd
maxefimov <efimov.academy.top@gmail.com>
2023-04-06 21:23:28 +0300

    06-04-23

--- OopFractionProject/OopFractionProject.cpp
+++ OopFractionProject/OopFractionProject.cpp
@@ -50,8 +50,74 @@ public:
         result.denominator = this->denominator * f.denominator;
         return result;
     }
+
+    bool Less(Fraction f)
+    {
+        return this->numerator* f.denominator < f.numerator * this->denominator;
+    }
+
+    friend Fraction operator+(Fraction f1, Fraction f2);
+    
+    friend Fraction operator+(Fraction f, int number);
+    friend Fraction operator+(int number, Fraction f);
+
+    friend bool operator<(Fraction f1, Fraction f2);
+    friend bool operator>(Fraction f1, Fraction f2);
+
+    friend bool operator==(Fraction f1, Fraction f2);
+
+    Fraction operator-()
+    {
+        return Fraction(-this->numerator, this->denominator);
+    }
+
+    Fraction operator++()
+    {
+        *this = *this + 1;
+        return *this;
+    }
+
+    Fraction operator++(int)
+    {
+
+    }
     
 };
+
+Fraction operator+(Fraction f1, Fraction f2)
+{
+    Fraction result;
+    result.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+    result.denominator = f1.denominator * f2.denominator;
+    return result;
+}
+
+Fraction operator+(Fraction f, int number)
+{
+    return f + Fraction(number, 1);
+}
+
+Fraction operator+(int number, Fraction f)
+{
+    return f + Fraction(number, 1);
+}
+
+bool operator<(Fraction f1, Fraction f2)
+{
+    return f1.numerator* f2.denominator < f2.numerator* f1.denominator;
+}
+
+bool operator>(Fraction f1, Fraction f2)
+{
+    return !(f1 < f2);
+}
+
+bool operator==(Fraction f1, Fraction f2)
+{
+    return f1.numerator * f2.denominator == f2.numerator * f1.denominator;
+}
+
+
 
 
 
@@ -65,6 +131,17 @@ int main()
     f1.Add(f2);
     cout << f1.ToString() << "\n";*/
 
-    cout << f1.Plus(f2).Minus(f3).ToString();
+    cout << f1.Plus(f2).Minus(f3).ToString() << "\n";
 
+    Fraction f4 = f1 + f2;
+
+    int n = 5;
+
+    Fraction f5 = -f1;
+
+    cout << boolalpha << (f1 > f2);
+    
+    cout << f3.ToString() << "\n";
+    ++f3;
+    cout << f3.ToString() << "\n";
 }
